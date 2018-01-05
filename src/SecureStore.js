8ebe2d0a6b29a78aee99e89ba676abd6c9378a85
Andreas Kraus <a.kraus@authensis.de>
2018-01-05 21:23:49 +0100

    Try fix undefined

--- src/SecureStore.js
+++ src/SecureStore.js
@@ -5,7 +5,7 @@ const debug = require('debug')('hap-client:securestore');
 function loadClient(clientName) {
     return Observable
         .defer(
-            () => Observable.of(null)
+            () => Observable.of({})
                 /*Keytar
                     .getPassword(clientName, 'clientInfo')*/
         )
@@ -30,7 +30,7 @@ function saveClient(self) {
 function load(clientName, username) {
     debug(`loading for ${clientName}/${username}`);
     return Observable
-        .of(null)
+        .of({})
         /*.from(
             Keytar
                 .getPassword(clientName, username)
